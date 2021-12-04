2c59bdaa7b03962a0fe68f328fe94169c2be0b0f
Kevin Fong <kevin_fong@berkeley.edu>
2021-12-04 12:16:26 -0800

    fixed namspace

---
+++ src/cellular_automaton.cxx
@@ -0,0 +1,19 @@
+#include <cellular_automata.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/* CONSTRUCTORS */
+CellularAutomaton::CellularAutomaton(
+    int n,
+    int m,
+    std::string boundary_conditions,
+    std::string rule,
+    bool sequential=true,
+    std::vector<int> initial_configuration=null;
+) {
+    std::cout << "Hello World! From CellularAutomaton constructor!" << std::endl;
+}
+
+
