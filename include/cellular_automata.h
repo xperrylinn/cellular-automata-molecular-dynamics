4b515776dfcab98967d55d23679d96a893fe252b
Xavier Linn <xperrylinn@berkeley.edu>
2021-12-04 11:48:16 -0800

    adding files

---
+++ include/cellular_automata.h
@@ -0,0 +1,84 @@
+#include <string>
+#include <vector>
+
+class CellularAutomaton {
+    private:
+        int n;  // Grid lenth
+        int m;  // Grid width
+        std::vector<int> current_state;
+        std::vector<vector<int>> snap_shots;
+        std::string boundary_conditions,
+        std::string rule; // Possible rule
+        bool sequential;
+        std::vector<int> initial_configuration=null;
+
+    public:
+        /* CONSTRUCTORS */
+        CellularAutomaton(
+            int n,
+            int m,
+            std::string boundary_conditions,
+            std::string rule,
+            bool sequential=true,
+            std::vector<int> initial_configuration=null;
+        );
+
+        /* STATE TRANITION FUNCTION */
+
+        /**
+         * state_transition_function
+         *
+         * Applies the rule function to each cell of the current state grid.
+         * 
+         * @param previous_state previous state grid of type vector<int>.
+         * @return null.
+         */
+        void state_transition_function(std::vector<int> previous_state);
+
+        /* HELPER FUNCTIONS */
+        int get_state
+
+        /* SNAP SHOT FUNCTION */
+        /**
+         * snap_shot
+         *
+         * TODO: fill in documentation here
+         * 
+         * @param state ???.
+         * @return null.
+         */
+        void snap_shot(std::vector<int> state);
+
+        /* RULES */
+
+        /**
+         * majority_rule
+         *
+         * TODO: fill in documentation here
+         * 
+         * @param  ???.
+         * @return ???.
+         */
+        void majority_rule();
+
+        /**
+         * purity_rule
+         *
+         * TODO: fill in documentation here
+         * 
+         * @param  ???.
+         * @return ???.
+         */
+        void purity_rule();
+
+        /**
+         * dispersion_rule
+         *
+         * TODO: fill in documentation here
+         * 
+         * @param  ???.
+         * @return ???.
+         */
+        void dispersion_rule();
+
+};
---
+++ src/cellular_automaton.cxx
@@ -0,0 +1,18 @@
+#include <cellular_automata.h>
+#include <string>
+#include <vector>
+
+using namespace CellularAutomaton;
+/* CONSTRUCTORS */
+CellularAutomaton(
+    int n,
+    int m,
+    std::string boundary_conditions,
+    std::string rule,
+    bool sequential=true,
+    std::vector<int> initial_configuration=null;
+) {
+    std::cout << "Hello World! From CellularAutomaton constructor!" << std::endl;
+}
+
+
