ed5a566d162641c7d06e648d1a3c450cb88d29e0
Kevin Fong <kevin_fong@berkeley.edu>
2021-12-17 21:29:52 -0800

    deleted extra tests in test_input

--- tests/test_input_services.cxx
+++ tests/test_input_services.cxx
@@ -13,57 +13,9 @@
 using namespace std;
 
 int main() {
-    cout << "This is the test suite for the input functions of the CellularAutomaton" << endl;
+    cout << "This is the test suite for the input functions of the CellularAutomaton" << endl << endl;
 
     // Constructor
-    vector<int> v;
-    v.push_back(0);
-    v.push_back(1);
-    v.push_back(0);
-    v.push_back(2);
-    v.push_back(1);
-    v.push_back(0);
-    v.push_back(1);
-    v.push_back(1);
-    v.push_back(0);
-
-    vector<string> boundary_conds_map_for_ca2 = {"periodic", "periodic", "periodic", "periodic"};
-
-    CellularAutomaton ca2 = CellularAutomaton(
-        3,
-        3,
-        boundary_conds_map_for_ca2,
-        "majority",
-        "VonNeumann",
-        v,
-        true
-    );
-    
-    // Test get last snap shot
-    vector<int> lsn = ca2.get_last_snapshot();
-
-    // Test print_grid()
-    ca2.print_grid(lsn);
-
-    // Test get_neighbors()
-    for (auto neighbor:ca2.get_neighbors(4))    {
-        cout<<neighbor<<" ";
-    }
-    cout<<endl;
-
-    for (auto neighbor:ca2.get_neighbors(0))    {
-        cout<<neighbor<<" ";
-    }
-    cout<<endl;
-
-    // Test Majority Rule()
-    ca2.majority_rule(3);
-
-    // Test state_transition_function()
-    ca2.state_transition_function();
-    ca2.print_current();
-
-    // Test parity_rule()
     vector<int> v3 = {
         0, 0, 0, 0,
         0, 1, 1, 0,
@@ -72,22 +24,6 @@ int main() {
     };
 
     vector<string> boundary_conds_map_for_ca3 = {"cutoff","cutoff", "periodic", "periodic"};
-
-    CellularAutomaton ca3 = CellularAutomaton(
-        4,
-        4,
-        boundary_conds_map_for_ca3,
-        "parity",
-        "VonNeumann",
-        v3,
-        true
-    );
-
-    ca3.print_current();
-    ca3.state_transition_function();
-    ca3.print_current();
-
-    ca3.write_snap_shots_to_csv("./testing_write_snap_shots_to_csv.csv");
 
     // Testing validating input parameters for n and m
     try{
@@ -171,5 +107,6 @@ int main() {
         std::cout << "Succesfully validated each periodic boundary has a complementary periodic boundary condition side" << std::endl;
     }
 
+    cout<<endl<<"End of test suite. All Functions perform as expected."<<endl;
     return 0;
 }
